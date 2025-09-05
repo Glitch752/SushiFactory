36f3604fdd7847af171c0463fbd64711af960d7c
Glitch752 <xxGlitch752xx@gmail.com>
2025-09-04 23:52:19 -0500

    Implement cutting board logic

--- world/interactable/cutting_board.gd
+++ world/interactable/cutting_board.gd
@@ -1,70 +1,76 @@
 extends "res://world/interactable/interactable.gd"
 
-#var cooking_time_remaining = 0.0
-#var has_rice = false
-#
-#const CUT_TIME = 10.0
-#
-#func _ready():
-    #set_process(false)
-    #super._ready()
-#
-#func interact():
-    #if !has_rice:
-        #var rice = PlayerInventorySingleton.remove_item()
-        #rice.queue_free()
-#
-        #has_rice = true
-        #cooking_time_remaining = COOK_TIME
-        #
-        #timer.start(cooking_time_remaining)
-        #set_process(true)
-    #elif cooking_time_remaining == 0:
-        #var rice = PlayerInventorySingleton.create_item(load("res://world/items/cooked_rice_item_data.tres"))
-        #PlayerInventorySingleton.try_grab_item(rice)
-        #has_rice = false
-        #_process(0)
-#
-#func _process(delta):
-    #cooking_time_remaining = max(cooking_time_remaining - delta, 0)
-    #if cooking_time_remaining == 0:
-        #set_process(false)
-    #
-    #var progress_bar: AnimatedSprite2D = $ProgressBar
-    #progress_bar.visible = cooking_time_remaining > 0
-    #if cooking_time_remaining > 0:
-        #var frame_count = progress_bar.sprite_frames.get_frame_count("default");
-        #progress_bar.frame = int((1.0 - cooking_time_remaining / COOK_TIME) * frame_count) % frame_count
-    #
-    #var output_indicator: Sprite2D = $OutputIndicator
-    #output_indicator.visible = cooking_time_remaining == 0 and has_rice
-#
-#
-#func can_interact() -> bool:
-    #if !has_rice:
-        #return PlayerInventorySingleton.holding_item("rice")
-    #elif cooking_time_remaining == 0:
-        #return !PlayerInventorySingleton.has_item()
-    #return false
-#
-#func get_interact_explanation():
-    #if !has_rice:
-        #return "add rice to the rice cooker"
-    #elif cooking_time_remaining == 0:
-        #return "take cooked rice from the rice cooker"
-    #return ""
-#
-#func get_interactable_name():
-    #return "Rice Cooker"
-#
-#func get_description():
-    #var extra = ""
-    #if has_rice:
-        #if cooking_time_remaining > 0:
-            #extra = "It's cooking rice, with %.1f seconds remaining." % cooking_time_remaining
-        #else:
-            #extra = "The rice is cooked and ready to take."
-    #else:
-        #extra = "It's empty."
-    #
-    #return "A rice cooker. " + extra
+const CUT_ITEMS = {
+    "cucumber": "sliced_cucumber",
+    "salmon": "sliced_salmon"
+}
+
+var cut_progress = 0
+var item = null
+
+func has_item() -> bool:
+    return item != null
+
+const CUTS_REQUIRED = 6.0
+
+func interact():
+    if !has_item():
+        var held_item = PlayerInventorySingleton.held_item_data()
+        if held_item and held_item.id in CUT_ITEMS.keys():
+            var new_item = PlayerInventorySingleton.remove_item()
+            
+            add_child(new_item)
+            item = new_item
+
+            cut_progress = 0
+    elif cut_progress < CUTS_REQUIRED:
+        cut_progress += 1
+
+        if cut_progress == CUTS_REQUIRED:
+            var sliced_item_id = CUT_ITEMS.get(item.data.id, null)
+
+            remove_child(item)
+            item.queue_free()
+            
+            var sliced_item = PlayerInventorySingleton.create_item(load("res://world/items/%s_item_data.tres" % sliced_item_id))
+            add_child(sliced_item)
+            
+            item = sliced_item
+    else:
+        PlayerInventorySingleton.try_grab_item(item)
+        item = null
+            
+        cut_progress = 0
+    
+    update_progressbar()
+
+func update_progressbar():
+    var progress_bar: AnimatedSprite2D = $ProgressBar
+    progress_bar.visible = has_item() and cut_progress < CUTS_REQUIRED
+    if has_item() and cut_progress < CUTS_REQUIRED:
+        var frame_count = progress_bar.sprite_frames.get_frame_count("default");
+        progress_bar.frame = int((cut_progress / CUTS_REQUIRED) * frame_count) % frame_count
+
+func can_interact() -> bool:
+    if !has_item():
+        var held_item = PlayerInventorySingleton.held_item_data()
+        return held_item and held_item.id in CUT_ITEMS.keys()
+    
+    return !PlayerInventorySingleton.has_item()
+
+func get_interact_explanation():
+    if !has_item():
+        return "place " + PlayerInventorySingleton.held_item_data().item_name.to_lower() + " on the cutting board"
+    elif cut_progress < CUTS_REQUIRED:
+        return "cut the " + item.data.item_name.to_lower()
+    else:
+        return "take the " + item.data.item_name.to_lower()
+
+func get_interactable_name():
+    return "Cutting Board"
+
+func get_description():
+    if has_item():
+        return "A cutting board with %s on it. You have cut it %d out of %d times." % [item.data.item_name.to_lower(), cut_progress, CUTS_REQUIRED]
+    else:
+        return "A cutting board. You can place certain items on it to cut them."
